adfc60d3bededd4510e10a2688bece88b921ff2e
thara <git@thara.dev>
2021-07-08 03:38:26 +0900

    :muscle: Implement chat clients

--- client/main.go
+++ client/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"fmt"
 	"grpc_chat/chat"
+	"io"
 	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 var (
@@ -28,6 +34,55 @@ func main() {
 	defer conn.Close()
 	client := chat.NewChatClient(conn)
 
+	id, myName := join(client, name)
+	fmt.Println("client ID: ", id)
+
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "client-id", strconv.FormatUint(id, 10))
+	stream, err := client.Messages(ctx)
+	if err != nil {
+		log.Fatalf("%v.Messages(_) = _, %v: ", client, err)
+	}
+
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				log.Fatalf("Failed to receive a note : %v", err)
+			}
+			fmt.Printf("\033[0G%s > %s\n", in.Recipient.Name, in.Message.Text)
+			fmt.Printf("%s > ", myName)
+		}
+	}()
+
+	fmt.Printf("%s > ", myName)
+
+	stdin := bufio.NewScanner(os.Stdin)
+	for stdin.Scan() {
+		text := stdin.Text()
+
+		if text == "exit" {
+			leave(client, id)
+			fmt.Println("bye.")
+			return
+		}
+
+		req := &chat.MessageRequest{
+			Id: id,
+			Message: &chat.Message{
+				Text: text,
+			},
+		}
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("Failed to send a note: %v", err)
+		}
+
+		fmt.Printf("%s > ", myName)
+	}
+}
+
+func join(client chat.ChatClient, name *string) (uint64, string) {
 	var chatName string
 	if name == nil {
 		chatName = "Unknown"
@@ -35,14 +90,21 @@ func main() {
 		chatName = *name
 	}
 
-	func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		resp, err := client.Join(ctx, &chat.JoinRequest{Name: chatName})
-		if err != nil {
-			log.Fatalf("%v.Join(_) = _, %v: ", client, err)
-		}
-		log.Println(resp)
-	}()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	resp, err := client.Join(ctx, &chat.JoinRequest{Name: chatName})
+	if err != nil {
+		log.Fatalf("%v.Join(_) = _, %v: ", client, err)
+	}
 
+	return resp.Id, chatName
+}
+
+func leave(client chat.ChatClient, id uint64) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	_, err := client.Leave(ctx, &chat.LeaveRequest{Id: id})
+	if err != nil {
+		log.Fatalf("%v.Leave(_) = _, %v: ", client, err)
+	}
 }
